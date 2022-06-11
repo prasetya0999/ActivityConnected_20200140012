778f392c2efab7c8b725da525a17bac59719a278
Prasetya Agung <prasetya.agung.ft20@mail.umy.ac.id>
2022-06-12 00:56:49 +0700

    Add project files.

---
+++ Connected/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Connected
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = GetConnectionString();
+            string query1 = "select * from Pembimbing_Akademik where NIK=333";
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd1 = new SqlCommand(query1, cn); cn.Open();
+                using (SqlDataReader dr1 = cmd1.ExecuteReader())
+                {
+                    while (dr1.Read())
+                    {
+                        string query2 = "UPDATE Pembimbing_Akademik SET Keahlian ='Jaringan' where NIK=333";
+                        SqlCommand cmd2 = new SqlCommand(query2, cn);
+                        cmd2.ExecuteNonQuery();
+                        Console.WriteLine("Data has been updated");
+                        Console.Read();
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+        private static string GetConnectionString()
+        {
+            return "Data Source=LAPTOP-C5A9RCLU\\PRASETYAAGUNGP;Initial Catalog=ProdiTI;Integrated Security=True";
+        }
+    }
+}
